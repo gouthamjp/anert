a4aa869febab0c46eef3fad7f8b2c33a74e579a3
ssabhijith2000 <ssabhijith2000@gmail.com>
2021-10-08 19:35:12 +0530

    OPTION SELECT FOR INSPECTION OR EV PAGE COMPLETED

--- lib/screens/option_selection.dart
+++ lib/screens/option_selection.dart
@@ -20,12 +20,13 @@ class _OptionSelectionState extends State<OptionSelection> {
   Widget build(BuildContext context) {
     final mquery = MediaQuery.of(context).size;
     return Scaffold(
+      appBar: GreenTvmTheme.themeAppbar(title: 'SMART TVM',  context: context, showBackButton: true),
       backgroundColor: Colors.white,
       body: SizedBox(
         height: mquery.height,
         width: mquery.width,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             StepperCounter(maxCount: 3,currentElement: 1,),
             Container(
@@ -40,19 +41,20 @@ class _OptionSelectionState extends State<OptionSelection> {
                     });
                   },
                   colour: optionSelect == Option.inspect? GreenTvmTheme.secondaryGray: Colors.white,
-                  cardChild: Text('SITE FOR INSPECTION'),)
+                  cardChild: Text('SITE FOR \nINSPECTION',textAlign: TextAlign.center,style: GreenTvmTheme.optionStyle,),)
                 ),
-                SizedBox(width: 30,),
+                SizedBox(width: 10,),
                 Expanded(
                   child: ReusableCard(
-                    onPress:  () {
-                    setState(() {
-                      optionSelect = Option.ev;
-                    });
-                  },
-                  colour: optionSelect == Option.ev? GreenTvmTheme.secondaryGray: Colors.white,
-                  cardChild: Text('SITE FOR EV'),)
-                ),
+                      onPress:  () {
+                      setState(() {
+                        optionSelect = Option.ev;
+                      });
+                    },
+                    colour: optionSelect == Option.ev? GreenTvmTheme.secondaryGray: Colors.white,
+                    cardChild: Text('SITE FOR \nEV',textAlign: TextAlign.center,style: GreenTvmTheme.optionStyle,),),
+                )
+
                 ],
               )
             ),
@@ -79,9 +81,13 @@ class ReusableCard extends StatelessWidget {
     return GestureDetector(
       onTap: onPress,
       child: Container(
+        alignment: Alignment.center,
+        height: 100,
         child: cardChild,
         margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(color: GreenTvmTheme.secondaryGray,borderRadius: new BorderRadius.all(Radius.elliptical(100, 50)),),
+        decoration: BoxDecoration(color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(color:GreenTvmTheme.secondaryGray,)),
         
       ),
     );
